239fe23b732aa30d00ffc7a747f29bdac4485875
Anjaligarhwal <anjaligarhwal1610@gmail.com>
2025-01-21 04:11:59 +0530

    Update app.js

--- 2-actual-backend/app.js
+++ 2-actual-backend/app.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://myntra-clone-frontened.onrender.com'); // Frontend URL
+
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
